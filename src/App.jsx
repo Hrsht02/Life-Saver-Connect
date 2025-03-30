0e7f39c1e5895884b9d4bcffb1164063f1d40ece
unknown <hrsht002@gmail.com>
2025-03-30 09:22:53 +0530

    Update Navbar import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Navbar from './navbar';
 import Homepage from './homepage';
 import Service from './services';
 import HealthMaintenance from './healthMaintenance';
 import LifeEnhancement from './lifeEnhancement';
 import About from './about';
 import Footer from './footer';
-import Navbar from './navbar';
+
 function App() { 
   return (
     <BrowserRouter>
@@ -17,7 +17,7 @@ function App() {
       <HealthMaintenance />
       <LifeEnhancement />
       <About />
-      <Footer />  {/* ✅ These components are always visible */}
+      <Footer /> 
     </BrowserRouter>
   );
 }
