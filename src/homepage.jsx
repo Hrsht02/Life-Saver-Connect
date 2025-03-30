6e66247c343bb78dedb4b5fc69572ad2a744d1e7
unknown <hrsht002@gmail.com>
2025-03-30 08:23:28 +0530

    Fixed case sensitivity and import issues

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,13 +1,13 @@
 import "./homePage.css";
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
 
 export default function Homepage() {
     return (
         <div>
             <Navbar />
             <div id='HomePageImage'>
-                <img src="/HomeImage.jpg" alt="Home Page" />  {/* ✅ Use imported image */}
+                <img src="/HomeImage.jpg" alt="Home Page" /> 
             </div>
         </div>
     );
