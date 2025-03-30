8ae711e26e4a479163f11e3a3fa49176e1e5db24
Harshit Kumar <157695197+Hrsht02@users.noreply.github.com>
2025-03-30 12:26:05 +0530

    Update healthMaintenance.jsx

--- src/healthMaintenance.jsx
+++ src/healthMaintenance.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./healthMaintenance.css";
+import "./HealthMaintenance.css";
 
 const HealthMaintenance = () => {
     const [bmiData, setBmiData] = useState({ weight: "", height: "", bmi: null });
